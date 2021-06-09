35375298efa0be8bf85fc97edb3f1b1c4e82d9f9
Gene Kelley <eugenek@guildmortgage.net>
2021-06-09 13:51:42 -0700

    Add functionality to add marker to the map.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import { render } from "react-dom";
 
 function App() {
   const [random, setRandom] = useState(Math.random());
+  const [marker, setMarker] = useState(); // default value is undefined
+
+  // Mock store locations from db.
+  const stores = {
+    central: [12.567898, 55.67583],
+    norrebro: [12.553806, 55.699299],
+    airport: [12.650784, 55.618042]
+  }
+
+  function handleStoreChange(event) {
+    const location = event.target.value;
+    marker.setLgnLat(stores[location]);
+  }
 
   mapboxgl.accessToken = "pk.eyJ1Ijoic3ZuYmxhbWUiLCJhIjoiY2twN2c1NHZpMDhobDJ3bGlkNjF0NWszaCJ9.go5qKdpS6kG93XxNP2v73w";
 
@@ -14,10 +27,24 @@ function App() {
       center: [12.567898, 55.675830], // Sets the center of the map (long, lat)
       zoom: 9
     });
+
+    // Add marker
+    const marker = new mapboxgl.Marker()
+      .setLgnLat([12.567898, 55.675830])
+      .addTo(map);
+
+      setMarker(marker);
   }, []);
 
   return <>
-    <button id="re-render" onClick={() => setRandom(Math.random())}>Re-render</button>
+    <div className='map-overlay'>
+      <h3>Choose Store: </h3>
+      <select onChange={handleStoreChange}>
+        <option value="central">Central Station</option>
+        <option value="norrebro">Norrebro street</option>
+        <option value="airport">CPH Airport</option>
+      </select>
+    </div>
     <div id="map"></div>
   </>;
 }
